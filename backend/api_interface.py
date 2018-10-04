78c87fd2ff1c08db403a5decffc45097651ec44b
Home Pi <pi@home.com>
2018-10-04 12:18:34 +1000

    Updated interfaces to spec

--- backend/api_interface.py
+++ backend/api_interface.py
@@ -10,133 +10,55 @@ app = Flask(__name__)
 app.debug = True
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
-def root():
-	# Dev method. If this is a static file, it is always best served via a web server.
-	# Full implementation will not use the flask interface to serve web pages
-	try:
-		with open('web/index.html', 'r') as f:
-			webpage = f.read()
-	except:
-		webpage = "File Error."
-		
-	return webpage
+
+###----------ORDERS----------###
+@app.route('/order', methods=['GET'])
+def route_get_order():
+        response_body, response_code = order_engine.get_order(request.form)
+        return jsonify(response_body), response_code
+
+        
+@app.route('/order', methods=['PUT'])
+def route_edit_order():
+        response_body, response_code = order_engine.edit_order(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/menu/all', methods=['GET'])
-def route_dishes():
-	return jsonify(mock_data.menu_item)
-	#return jsonify(me.get_all_dishes())
+@app.route('/order/all', methods=['GET'])
+def route_get_order():
+        response_body, response_code = order_engine.get_all_orders(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/menu/active', methods=['GET'])
-@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
-def route_menu():
-	response = []
-	for item in mock_data.menu_item:
-		if item['status'] == 'active':
-			response.append(item)
-
-	return jsonify(response)
-	#return jsonify(me.get_menu())
+###----------MENU----------###
+@app.route('/menu', methods=['POST'])
+def route_set_menu():
+        response_body, response_code = menu_engine.set_menu(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/menu/dish', methods=['GET'])
-def route_item():
-	for item in mock_data.menu_item:
-		if item['id'] == int(request.args.get('id')):
-			return jsonify(item)
-
-	return jsonify("Does not exist"), 400
-	#return jsonify(me.get_by_itemid(request.args.get('id')))
+@app.route('/menu', methods=['GET'])
+def route_get_menu_item():
+        response_body, response_code = menu_engine.get_menu_item(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/menu/add', methods=['GET'])
-def add_item():
-	item = {
-		"id": int(request.args.get('id')),
-		"name": request.args.get('name'),
-		"type": request.args.get('type'),
-		"description": request.args.get('desc'),
-		"image": request.args.get('img'),
-		"base_price": float(request.args.get('base')),
-		"status": request.args.get('stat')
-	}
-
-	mock_data.menu_item.append(item)
-	return jsonify(mock_data.menu_item)
+@app.route('/menu', methods=['PUT'])
+def route_edit_menu_item():
+        response_body, response_code = menu_engine.edit_menu_item(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/menu/remove', methods=['GET'])
-def remove_item():
-	for item in mock_data.menu_item:
-		if item['id'] == int(request.args.get('id')):
-			mock_data.menu_item.remove(item)
-			return jsonify(mock_data.menu_item)
-
-	return jsonify("Does not exist"), 400
+@app.route('/menu/all', methods=['POST'])
+def route_get_all_menu():
+        response_body, response_code = menu_engine.get_all_menu(request.form)
+        return jsonify(response_body), response_code
 
 
-@app.route('/api/users', methods=['GET'])
-def get_users():
-	return UserFacade.get_users(request)
-
-
-@app.route('/api/users', methods=['POST'])
-def add_user():
-	return UserFacade.add_user(request)
-
-
-@app.route('/api/orders', methods=['GET'])
-def get_orders():
-	return OrderFacade.get_orders(request)
-
-
-@app.route('/api/orders/<orderId>', methods=['GET'])
-def get_order(orderId):
-	return OrderFacade.get_order(request, orderId)
-
-
-@app.route('/api/orders/admin', methods=['GET'])
-def get_orders_admin():
-	return(jsonify(mock_data.orders_data)), 200
-
-
-@app.route('/api/orderitems', methods=['GET'])
-def get_order_items():
-	return(jsonify(mock_data.order_items_data)), 200
-
-
-@app.route('/api/orders/kitchen', methods=['GET'])
-def get_orders_kitchen():
-	return(jsonify(mock_data.order_kitchen_data)), 200
-
-
-@app.route('/api/orders/ready', methods=['GET'])
-def get_orders_ready():
-	return(jsonify(mock_data.order_ready_data)), 200
-
-@app.route('/api/example/<path_param>', methods=['GET','POST','PUT','DELETE'])
-def example_route(path_param):
-	# Make the call http://0.0.0.0:5000/api/example/recipes?query_param_1=soup&query_param_2=tasty
-	#
-	# query_param_1 and query_param_2 are passed as 'query strings'
-	# query strings are used to 'query' a resource
-	#
-	# 'recipes' is a path parameter (passed as a method parameter of the route)
-	# path parameters are used to represent resources and subresources, like a file system
-	# e.g. '/Documents/users.csv' resembles '/api/users'
-
-	query_param_1 = request.args.get("query_param_1", None) # where None is the default value
-	query_param_2 = request.args.get("query_param_2", None)
-
-	return(jsonify([query_param_1, query_param_2, path_param, request.method]), 200)
-
-#all reservations
-@app.route('/api/reservations', methods=['GET'])
-def get_reservations_all():
-	return ReservationFacade.get_reservations(request)
-
+@app.route('/menu', methods=['POST'])
+def route_set_menu():
+        response_body, response_code = menu_engine.set_menu(request.form)
+        return jsonify(response_body), response_code
 
 
 if __name__ == "__main__":
