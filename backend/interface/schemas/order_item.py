dba1e57dc9b21d4019c110c2cc2a752b822008eb
LIJIAN CHEN <97108258>
2018-10-04 12:57:26 +1000

    add OrderItemSchema

--- backend/interface/schemas/order_item.py
+++ backend/interface/schemas/order_item.py
@@ -6,3 +6,13 @@ sys.path.insert(0, '../../')
 from db.schemas.order_item import OrderItem
 
 # todo: Order Item object schema
+class OrderItemSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    slot = fields.Int()
+    price = fields.Float()
+    status = fields.Str()
+    order_type = fields.Str()
+    menu_item_id = fields.Integer(dump_only=True)
+    order_id = fields.Integer(dump_only=True)
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime(dump_only=True)
