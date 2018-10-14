77ea1a4da57e71acea777f57b140149c7a73fa0b
Alaswad <Wallace.Alaswad@emc.com>
2018-10-14 12:03:26 +1100

    get_All_Order request

--- backend/src/order_engine.py
+++ backend/src/order_engine.py
@@ -1,7 +1,9 @@
 from base import session_factory
 from .decorators import error_handler
 from db.schemas.order import Order
+from db.schemas.order_item import OrderItem
 from interface.schemas.order import OrderSchema
+from interface.schemas.order_item import OrderItemSchema
 
 # Business logic for the order module
 
@@ -17,6 +19,7 @@ def get_order(request):
 		order, errors = schema.dump(order_object)
 	except Exception as error:
 		return (error)
+	
 
 	session.close()
 	return order, 200
@@ -28,10 +31,18 @@ def edit_order(data):
 @error_handler
 def get_all_orders(request):
 	# todo: Use url query params to filter in db query
+	OrderItemStatus = string_to_bool(request.args.get("Status", None))
 	session = session_factory()
 
 	order_objects = session.query(Order).all()
 	schema = OrderSchema(many=True)
+	orders, errors = schema.dump(order_objects)
+
+	if OrderItemStatus != None:
+		order_objects = session.query(OrderItem).filter(OrderItem.Status == OrderItemStatus)
+	else:
+		order_objects = session.query(OrderItem).all()
+
 	orders, errors = schema.dump(order_objects)
 
 	session.close()
