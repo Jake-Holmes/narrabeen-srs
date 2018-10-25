3339ce1dc769916f363a244fae0fb8fdb8ac7b7d
Nicholas Schuler <nicholas.schuler@student.uts.edu.au>
2018-10-25 14:04:36 +1100

    Implemented Basic Shopping Cart to Replace Ben's temporary one. Shopping cart is accessible by the left hand sidebar.

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from '.././app-routing.module';
+import { AppComponent } from '.././app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NoProductsFoundComponent } from "./no-products-found/no-products-found.component";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NoAccessComponent } from './no-access/no-access.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoProductsFoundComponent,
+    PageNotFoundComponent,
+    NoAccessComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
+  ],
+  exports: [
+    NoProductsFoundComponent,
+    NoAccessComponent,
+    PageNotFoundComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class SharedModule { }
