ee658f59cc6a6d1b00bdc5449b979e25818975df
Ben Bromley <ben@inspiredsoftware.com.au>
2018-11-06 21:45:46 +1100

    WIP: File uploader

--- frontend/src/app/menu-item-admin/menu-item-admin.component.ts
+++ frontend/src/app/menu-item-admin/menu-item-admin.component.ts
@@ -12,6 +12,8 @@ import { MenuService } from '../menu.service';
 export class MenuItemAdminComponent implements OnInit {
 
   item: MenuItem;
+  image: string;
+  base64textString: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -27,11 +29,32 @@ export class MenuItemAdminComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
 
     if (id) {
-      this.menuService.getMenuItem(id).subscribe(menuItem => this.item = menuItem);
+      this.menuService.getMenuItem(id).subscribe(menuItem => {
+        this.item = menuItem;
+        this.image = this.item.image;
+      });
     } else {
       this.item = new MenuItem;
     }
   }
+
+  onImageChange(event): void {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(event.target.files[0]);
+    }
+  }
+
+  _handleReaderLoaded(event): void {
+    const binaryString = event.target.result;
+    this.base64textString = btoa(binaryString);
+    console.log(btoa(binaryString));
+  }
+
+  // UploadImage(): {
+
+  // }
 
   Save(): void {
     if (this.item.id) {
