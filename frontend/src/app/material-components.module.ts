1099b91d5efa5a39cebc0ad9c382369e834cb5cf
Ben Bromley <ben@inspiredsoftware.com.au>
2018-11-06 19:38:25 +1100

    FEAT: Update admin view

--- frontend/src/app/material-components.module.ts
+++ frontend/src/app/material-components.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatSnackBarModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatListModule],
-  exports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatListModule]
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatListModule, MatSnackBarModule],
+  exports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatListModule, MatSnackBarModule]
 })
 export class MaterialComponentsModule { }
