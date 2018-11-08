12102f0877d32b7b29bfd6a9abc9a3eb6350d6ef
jordanlam <jordan.lam95@gmail.com>
2018-11-09 00:23:18 +1100

    Get customer reservation from phone number is implemented.

--- backend/src/customer_engine.py
+++ backend/src/customer_engine.py
@@ -1,6 +1,10 @@
 from .decorators import error_handler
 from .common_functions import string_to_bool, session_scope
 from db.schemas.customer import Customer
+from db.schemas.reservation import Reservation
+# from db.schemas.takeawayorder import Takeawayorder
+# from interface.schemas.takeawayorder import TakeawayorderSchema
+from interface.schemas.reservation import ReservationSchema
 from interface.schemas.customer import CustomerSchema
 from sqlalchemy import exists
 
@@ -39,11 +43,30 @@ def get_customer(request):
 
 @error_handler
 def get_takeaway_customer(request):
+    # param_phone = request.args.get("mobNum")
+    # schema = TakeawayorderSchema(many=True, exclude=("order.takeaway",))
+    #
+    # with session_scope() as session:
+    #     customer_object = session.query(Customer).filter(Customer.phone == param_phone).scalar()
+    #     # Search for customer id in takeawayorder
+    #     customer_takeaway = session.query(Takeawayorder).filter(Takeawayorder.customer_id == customer_object.id)
+    #     takeawayorder_item, errors = schema.dump(customer_takeaway)
+    #
+    # return takeawayorder_item, 200
     return 200
 
 @error_handler
 def get_customer_reservation(request):
-    return 200
+    param_phone = request.args.get("mobNum")
+    schema = ReservationSchema(many=True, exclude=("customer",))
+
+    with session_scope() as session:
+        customer_object = session.query(Customer).filter(Customer.phone == param_phone).scalar()
+        # Search for customer id in reservation
+        customer_reservation = session.query(Reservation).filter(Reservation.customer_id == customer_object.id)
+        reservation_item, errors = schema.dump(customer_reservation)
+
+    return reservation_item, 200
 
 @error_handler
 def edit_customer(request):
