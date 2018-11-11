1519987c8a093c6c751be4fd93ec4246b43f2f50
Jonathan-PC\Jonathan <jonathan.j.ho@student.uts.edu.au>
2018-11-11 18:45:53 +1100

    3 toolbar navigation added. Everyone needs to update this bar for and component for correct navigation.

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -8,27 +8,41 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent implements OnInit {
 
-  private privilaged: Boolean;
+  //Identifiers for each page. Tells site which type of page is currently displayed. Determiens which icons will be displayed on site.
 
-  private privilagedUrl: string[];
+  private staffPage: Boolean;
 
-  private unprivilagedUrl: string[];
+  private tablePage: Boolean;
+
+  private sitePage: Boolean;
+
+  //Stored string to be compared with current URLs. Contains all URLs, determines what toolbar is required for each page.
+
+  private staffUrl: string[];
+
+  private siteUrl: string[];
+
+  private tableUrl: string[];
 
   private noToolBarUrl: string[];
 
   constructor(
     private router: Router,
   ) {
-    //To display correct poolbar icons, you need to add the router link the these arrays. If its a page that does not require permissions, put the url into this.unprivilagedUrl.
-    //If the page appears after the user has logged in either as staff or admin, put your router link in privilagedUrl.
+    //To display correct poolbar icons, you need to add the router link the these arrays. If its a page that does not require permissions, put the url into this.siteUrl.
+    //If the page appears after the user has logged in either as staff or admin, put your router link in staffUrl.
+    //If the page appears after the user has scanned the Table QR code put router link into tableUrl.
     //If your page does not have a toolbar, put your link in noToolBarUrl.
     //If your page is too hard to put in, this module is specifically designed so that AS LONG AS THE PREVIOUS PAGE THE USER WAS ON HAS THE CORRECT TOOLBAR, the correct toolbar will show in the page.
     //Howevr like vaccines and herd immunity. if you can easily put your router link in, put it in.
+    //Module will match only the first characters of the URLs. That means if you have a site like: /menu/login or /menu?id=2, you can jsut put in /menu and the module will work.
+  
 
-    this.unprivilagedUrl=['']
-    this.privilagedUrl=['/kitchen-view', '/staffHome', '/tablemanagement', '/tableManagement-create', '/kitchen-view', '/checkout']
-    this.noToolBarUrl=['/tableLogin', '/staffLogin', '/tableLanding']
-   }
+    this.siteUrl = ['/']
+    this.staffUrl = ['/kitchen-view', '/staffHome', '/tablemanagement', '/tableManagement-create', '/kitchen-view', '/checkout']
+    this.tableUrl = []
+    this.noToolBarUrl = ['/tableLogin', '/staffLogin', '/tableLanding']
+  }
 
   ngOnInit() {
   }
@@ -43,34 +57,49 @@ export class SidebarComponent implements OnInit {
   };
 
   public showToolbar(): Boolean {
-    if (this.noToolBarUrl.indexOf(this.router.url) !== -1) {  //first check if toolbar is displayed.
+    if (this.noToolBarUrl.indexOf(this.router.url) == 1 || this.noToolBarUrl.indexOf(this.router.url) == 0) {  //first check if toolbar is displayed.
       return false;
     }
-    if (this.setPrivilages(this.privilagedUrl, this.router.url)) { //if toolbar is displayed check for privilages then return false.
-      this.privilaged = true;
+    if (this.setPrivilages(this.staffUrl, this.router.url)) { //if toolbar is displayed check for privilages then return false.
+      this.staffPage = true;
+      this.tablePage = false;
+      this.sitePage = false;
       return true;
-    } else if (this.setPrivilages(this.unprivilagedUrl, this.router.url)){
-      this.privilaged = false;
+    } else if (this.setPrivilages(this.siteUrl, this.router.url)) {
+      this.staffPage = false;
+      this.tablePage = false;
+      this.sitePage = true;
+      return true;
+    } else if (this.setPrivilages(this.tableUrl, this.router.url)) {
+      this.staffPage = false;
+      this.tablePage = true;
+      this.sitePage = false;
       return true;
     } else {
       return true;
     }
   }
 
-  private setPrivilages(array: string[], url:string) {
-    if (array.indexOf(url) !== -1) {
+  private setPrivilages(array: string[], url: string) {
+    if (array.indexOf(url) == 1 || array.indexOf(url) == 0) {
+      var s  = array.indexOf(url);
       return true;
     } else {
+      var s  = array.indexOf(url);
       return false;
     }
   }
 
-  public isNotPrivilaged() {
-    return !this.privilaged;
+  public isStaff() {
+    return this.staffPage;
   }
 
-  public isPrivilaged() {
-    return this.privilaged;
+  public isTable() {
+    return this.tablePage;
+  }
+
+  public isSite() {
+    return this.sitePage;
   }
 
 }
