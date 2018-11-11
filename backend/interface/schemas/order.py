184a3ada27570ccf174a563e0c802f86fa08e8b0
Corey Stidston <corey.stidston@student.uts.edu.au>
2018-11-12 10:21:57 +1100

    get customer takeaway returns single takeaway order instead of collection 'fixed' issue with removing enum prefixes with some janky A S

--- backend/interface/schemas/order.py
+++ backend/interface/schemas/order.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, pre_dump
+from marshmallow import Schema, fields, post_dump
 import sys
 sys.path.insert(0, '../../')
 from db.schemas.order import Order
@@ -12,8 +12,8 @@ class OrderSchema(Schema):
 	date_created = fields.DateTime(dump_only=True, required=True) # Ignore date_created when loading data to object
 	date_modified = fields.DateTime(dump_only=True, required=True) # Ignore data_modified when loading data to object
 
-	@pre_dump
-	def remove_enum_prefix(self, in_data):
-		in_data.order_type = in_data.order_type.name
-		in_data.status = in_data.status.name
-		return in_data
+	@post_dump
+	def remove_enum_prefix(self, out_data):
+		out_data["status"] = out_data["status"].replace("OrderStatus.","")
+		out_data["order_type"] = out_data["order_type"].replace("OrderType.","")
+		return out_data
