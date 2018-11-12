235a8c690de143068aa60edce4289f4125cf7359
Ye Yuan <y441572563@gmail.com>
2018-11-13 00:52:52 +1100

    fixed the error
    fixed the error

--- frontend/src/app/checkout/checkout.module.ts
+++ frontend/src/app/checkout/checkout.module.ts
@@ -1,9 +1,9 @@
-import { CheckoutRoutingModule } from "./checkout.routing";
-import { SharedModule } from "./../shared/shared.module";
-import { CheckoutNavbarComponent } from "./checkout-navbar/checkout-navbar.component";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { CheckoutComponent } from "./checkout.component";
+import { CheckoutRoutingModule } from './checkout.routing';
+import { SharedModule } from './../shared/shared.module';
+import { CheckoutNavbarComponent } from './checkout-navbar/checkout-navbar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
 import { BillingDetailsComponent } from './billing-details/billing-details.component';
 import { ResultComponent } from './result/result.component';
 
